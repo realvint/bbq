d9a88d3e66f53b789992e0c1ed36f8292f496a9b
realvint <victor.malukhin@gmail.com>
2021-12-18 12:50:04 +0300

    add user to events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :events
+
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
