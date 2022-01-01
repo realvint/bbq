071797da616a20ea69f8ca52e4b4841c77fa1adb
realvint <victor.malukhin@gmail.com>
2022-01-02 00:23:21 +0300

    update footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def user_avatar(user)
     asset_path('user.png')
   end
+
+  def github_url(author, repo)
+    link_to 'BBQ', "https://github.com/#{author}/#{repo}", target: '_blank'
+  end
 end
