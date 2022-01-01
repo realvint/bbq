56dbbb929632b73d4aba6ec5527e4fd944d309c8
realvint <victor.malukhin@gmail.com>
2022-01-02 00:47:14 +0300

    add name field to registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   helper_method :current_user_can_edit?
 
   def current_user_can_edit?(event)
     user_signed_in? && event.user == current_user
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
