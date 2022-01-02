23469bab67be809960ec8cca14dc8ec4d4205227
realvint <victor.malukhin@gmail.com>
2022-01-02 16:30:02 +0300

    feature: destroy user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
 
-  has_many :events
+  has_many :events, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 35 }
 end
