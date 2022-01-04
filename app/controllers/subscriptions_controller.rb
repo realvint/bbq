def144591fcd81b8559eb5815555470d44d698c1
realvint <victor.malukhin@gmail.com>
2022-01-04 21:40:35 +0300

    implement subscriptions

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,38 @@
+class SubscriptionsController < ApplicationController
+  before_action :set_event, only: %i[create destroy]
+
+  def create
+    @new_subscription = @event.subscriptions.build(subscription_params)
+    @new_subscription.user = current_user
+
+    if @new_subscription.save
+      redirect_to @event, notice: t('.created')
+    else
+      render 'events/show', alert: t('.error')
+    end
+  end
+
+  def destroy
+    @subscription = @event.subscriptions.find(params[:id])
+
+    message = { notice: t('.destroyed') }
+
+    if current_user_can_edit?(@subscription)
+      @subscription.destroy
+    else
+      message = { alert: t('.error') }
+    end
+
+    redirect_to @event, message
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def subscription_params
+    params.fetch(:subscription, {}).permit(:user_email, :user_name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: %i[show edit update]
   resources :events do
     resources :comments, only: %i[create destroy]
+    resources :subscriptions, only: %i[create destroy]
   end
 end
