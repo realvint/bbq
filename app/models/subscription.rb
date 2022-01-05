937b96f51a83a297ca57a1f4f57ac6820244ce6d
realvint <victor.malukhin@gmail.com>
2022-01-05 12:21:40 +0300

    add user_email validate before subscribing to event

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,11 +2,14 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
+  before_validation :email_downcase
+
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, unless: -> { user.present? }
   validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
   validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
   validate :self_event, if: -> { user.present? }
+  validate :email_exist, unless: -> { user.present? }
 
   def user_name
     user&.name || super
@@ -18,7 +21,15 @@ class Subscription < ApplicationRecord
 
   private
 
+  def email_downcase
+    user_email&.downcase!
+  end
+
   def self_event
     errors.add(:user, :error_self_event) if event.user == user
+  end
+
+  def email_exist
+    errors.add(:user_email, :error_email_exist) if User.find_by(email: user_email)
   end
 end
