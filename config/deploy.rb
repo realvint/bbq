4ebbb246e6251f8172d5b6f04b7d1551cc0e4682
realvint <victor.malukhin@gmail.com>
2022-01-24 23:52:56 +0300

    update deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, 'bbq'
 set :repo_url, 'git@github.com:realvint/bbq.git'
 set :deploy_to, -> { "/home/deploy/projects/#{fetch(:application)}" }
 set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/packs public/uploads node_modules]
-set :linked_files, %w[config/database.yml]
+set :linked_files, %w[config/database.yml config/master.key]
 set :user, 'deploy'
 set :keep_releases, 5
 set :branch, ENV['BRANCH'] if ENV['BRANCH']
