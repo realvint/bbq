87b6ef911c1704b6476020118a7200c43c23aa5c
realvint <victor.malukhin@gmail.com>
2022-01-27 00:35:41 +0300

    add omniauth github

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,25 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def github
+    handle_auth 'Github'
+  end
+
   def google_oauth2
+    handle_auth 'Google'
+  end
+
+  def handle_auth(kind)
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      flash[:notice] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+      flash[:notice] = t 'devise.omniauth_callbacks.success', kind: kind
       sign_in_and_redirect @user, event: :authentication
     else
-      session['devise.google_data'] = request.env['omniauth.auth'].except('extra')
+      session['devise.auth_data'] = request.env['omniauth.auth'].except('extra')
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
+  end
+
+  def failure
+    redirect_to root_path, alert: t('devise.omniauth_callbacks.failure')
   end
 end
